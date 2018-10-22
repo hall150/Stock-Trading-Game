a6750ef801275aac8bc9ac4a439592a785736372
Cesar McFarlin <camcfarlin@me.com>
2018-10-22 11:53:07 -0600

    Delete dev.js
    only for dev

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-//dev.js - dont commit this!!!
-module.exports = {
-    googleClientID: '212536734862-ul4ifvh01nklo4ccrtfsjhhk8u390jhq.apps.googleusercontent.com',
-    googleClientSecret: 'tZ9kUdXYTMgtNwbb2ZsmOhtM',
-    mongoURI: 'mongodb://camcfarlin:hm7X4C2XJ4V9LnF@ds235243.mlab.com:35243/clients',
-    cookieKey: 'efj3o4jf98jfafjw39fj93j9f39f39j',
-    alphaVantageKey: '1IZWJYO543SGPYUG',
-};
